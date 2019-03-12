995d7236791d7584e32a9d1b723d678afda231fc
Paul Prescod <pprescod@salesforce.com>
2019-03-11 18:01:25 -0700

    WIP

--- sfdo_template_helpers/admin/middleware.py
+++ sfdo_template_helpers/admin/middleware.py
@@ -17,9 +17,12 @@ class AdminRestrictMiddleware:
 
     def __call__(self, request):
         restricted_areas = tuple(getattr(settings, "RESTRICTED_PREFIXES", ()))
-        restricted_areas += tuple([getattr(settings, "ADMIN_AREA_PREFIX", ())])
+        admin_area = getattr(settings, "ADMIN_AREA_PREFIX", None)
+        if admin_area is not None:
+            restricted_areas += (admin_area,)
 
         for area in restricted_areas:
+            print(request.path, "VS", area, "FROM", restricted_areas)
             if request.path.startswith(f"/{area}"):
                 self._validate_ip(request)
 
@@ -28,6 +31,8 @@ class AdminRestrictMiddleware:
     def _validate_ip(self, request):
         ip_str = get_remote_ip(request)
         ip_addr = IPv4Address(ip_str)
+
+        print("Validating", ip_addr, "Vs", self.ip_ranges)
 
         if not any(ip_addr in subnet for subnet in self.ip_ranges):
             raise SuspiciousOperation(f"Disallowed IP address: {ip_addr}")
