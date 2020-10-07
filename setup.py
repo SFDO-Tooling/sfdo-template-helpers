3fc7a05fe474ab2bb707e48dfd924f31a06c2611
David Glick <dglick@salesforce.com>
2020-10-07 10:43:10 -0400

    Drop support for python 3.7 and Django 2.1, fix test

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         "markdown (>=3.0,<4.0)",
         "bleach (>=3.0,<4.0)",
-        "django (>=2.1,<4.0)",
+        "django (>=2.2,<4.0)",
         "djangorestframework (>=3.9,<4.0)",
         "cryptography (>=2.5,<4.0)",
         "django-filter (>=2.1,<3.0)",
