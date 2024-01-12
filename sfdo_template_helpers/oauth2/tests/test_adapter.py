339da0ab413c0d1936e0a6b4bde5b0182b4baa77
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2024-01-11 16:40:26 -0800

    Fixing test failures on_authentication_error for DefaultSocialAccountAdapter

--- sfdo_template_helpers/oauth2/tests/test_adapter.py
+++ sfdo_template_helpers/oauth2/tests/test_adapter.py
@@ -6,12 +6,12 @@ from ..adapter import SFDOSocialAccountAdapter
 
 def test_authentication_error_logs(mocker):
     mocker.patch(
-        "allauth.socialaccount.adapter.DefaultSocialAccountAdapter.authentication_error"
+        "allauth.socialaccount.adapter.DefaultSocialAccountAdapter.on_authentication_error"
     )  # noqa
     error = mocker.patch("sfdo_template_helpers.oauth2.adapter.logger.error")
     adapter = SFDOSocialAccountAdapter()
 
     with pytest.raises(exceptions.ConnectionError):
-        adapter.on_authentication_error(exception=exceptions.ConnectionError)
+        adapter.authentication_error(exception=exceptions.ConnectionError)
 
     assert error.called
