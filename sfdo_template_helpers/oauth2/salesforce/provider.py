745758a51f45215b6e145c8578cc5fdc17ee130b
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2024-09-04 21:56:52 -0700

    @W-16662200 - fix auth params code refactor

--- sfdo_template_helpers/oauth2/salesforce/provider.py
+++ sfdo_template_helpers/oauth2/salesforce/provider.py
@@ -4,7 +4,7 @@ from allauth.socialaccount.providers.salesforce.provider import SalesforceProvid
 class SFDOSalesforceProvider(SalesforceProvider):
     package = "sfdo_template_helpers.oauth2.salesforce"
 
-    def get_auth_params(self, request, action):
+    def get_auth_params_from_request(self, request, action):
         ret = super().get_auth_params_from_request(request, action)
         # This will ensure that even if you're logged in to Salesforce,
         # you'll be prompted to choose an identity to auth as:
